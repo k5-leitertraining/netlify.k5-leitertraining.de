cdf85bfb9e925dd1d58e5287bd949a07aef4717f
Kai Wittmann <kai@heart-co.de>
2025-03-04 22:39:00 +0100

    make sure error redirect points to a valid page

--- etc/reveal-plan/index.js
+++ etc/reveal-plan/index.js
@@ -4,7 +4,7 @@ const availableTargets = {
   'einflussnehmerkursQ2-24': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=552238&bundle_token=1009be4b4305f98b87a7187690fa2d79&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -21,7 +21,7 @@ const availableTargets = {
   'leiterkurs01_24-09-16': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610751&bundle_token=98e55edaf9e120b832e515a870065867&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -38,7 +38,7 @@ const availableTargets = {
   'leiterkurs02_24-09-17': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610752&bundle_token=6929ebf9a7458c37bfe20ad269b5785a&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -55,7 +55,7 @@ const availableTargets = {
   'leiterkurs03_24-09-18': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610753&bundle_token=611aa9f7db50dab19eda4289aba235e0&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -72,7 +72,7 @@ const availableTargets = {
   'leiterkurs04_24-09-19': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610754&bundle_token=a7a00011d63f1ccd7316b5d16a5dff7b&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -89,7 +89,7 @@ const availableTargets = {
   'leiter-dna-kurs_24-09-12': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610756&bundle_token=7e2879381c99b284b558f7de260b6f84&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -106,7 +106,7 @@ const availableTargets = {
   'einflussnehmer-summer-school_24-06-10': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610749&bundle_token=7eb5ee7fe2fac2bb35db13b7dab0fe5b&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -123,7 +123,7 @@ const availableTargets = {
   'leiter-dna-kurs_25-01-21': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1690201&bundle_token=d69a0ab2a1a86dd7b99bc7521353d8de&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -140,7 +140,7 @@ const availableTargets = {
   'leiterkurs01_25-01-27': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1690207&bundle_token=cfef5884222354366a01696fe0f66727&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -157,7 +157,7 @@ const availableTargets = {
   'leiterkurs02_25-02-04': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1690208&bundle_token=082607ef14e2113fdb575e1ccd5bb233&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -174,7 +174,7 @@ const availableTargets = {
   'leiterkurs03_25-02-05': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1690209&bundle_token=c44d2d9c0d46c3886dafacc8be2c2b51&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -191,7 +191,7 @@ const availableTargets = {
   'leiterkurs04_25-02-06': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=610754&bundle_token=a7a00011d63f1ccd7316b5d16a5dff7b&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -208,7 +208,7 @@ const availableTargets = {
   'leiter-dna-kurs': {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1768733&bundle_token=71c86ae33abcd7ee81f2bcf579eb5669&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -225,7 +225,7 @@ const availableTargets = {
   leiterkurs01: {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1772277&bundle_token=47e70773c2c3b2d2d47e4c391b966440&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -242,7 +242,7 @@ const availableTargets = {
   leiterkurs02: {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1772279&bundle_token=f7ec79ac2fc5f8f2d6428b6b8282afcd&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -259,7 +259,7 @@ const availableTargets = {
   leiterkurs03: {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1772284&bundle_token=10a7dc6860c1b61261fb34dc66923131&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
@@ -276,7 +276,7 @@ const availableTargets = {
   leiterkurs04: {
     urlToRedirect:
       'https://onecommunity.mn.co/users/onboarding/choose_plan?plan_id=1772285&bundle_token=41a5bc9a5cc1701a19fb152792f8a2e2&prefer_signup=true&utm_source=manual',
-    urlToError: '/error-outdated-link',
+    urlToError: 'https://k5-leitertraining.de/error',
     checks: [
       // He should have the course in "Aktuelle Kurse"
       {
