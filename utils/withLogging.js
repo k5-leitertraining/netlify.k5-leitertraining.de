38539437f18b6df86a524555d37464260adc1e7b
Kai Wittmann <kai@heart-co.de>
2026-02-03 19:29:56 +0100

    chore: add body logging

--- utils/withLogging.js
+++ utils/withLogging.js
@@ -3,6 +3,7 @@ const withLogging =
   async (...args) => {
     const [event] = args
     console.log(`Request Received: ${event.httpMethod} ${event.rawUrl}`)
+    console.log(`Request Body:`, event.body)
     const result = await handler(...args)
     console.log(`Result of ${event.httpMethod} ${event.rawUrl}:`, result)
     return result
